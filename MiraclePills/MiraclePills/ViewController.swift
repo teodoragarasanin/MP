71603f9447fd27c07a98c40864f241b6a8825bdf
Teodora <garasanin.teodora@gmail.com>
2016-12-29 19:41:03 +0100

    UIPickerView finished, data source and delegate

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -8,10 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    //MARK: Outlets
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var stateButton: UIButton!
+   
+    //MARK: Properties
+    
+    let states = ["Alaska", "California", "New York"]
+    
+    //MARK: Actions
+    
+    @IBAction func chooseStateButtonTapped(_ sender: UIButton) {
+        
+        statePicker.isHidden = false
+        
+    }
+    
+    //MARK: UIPickerViewDelegate
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        stateButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    
+    
+    
+    // MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
