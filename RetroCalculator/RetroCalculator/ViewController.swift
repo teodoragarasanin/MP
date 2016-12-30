ab311220c8500983bef96f482524fce985b4be59
Teodora <garasanin.teodora@gmail.com>
2016-12-30 17:01:17 +0100

    RetroCalculatorApp. StackViews.

---
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Teodora on 12/30/16.
+//  Copyright © 2016 Teodora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
