4d139377173f4d190c7161e7bc3dad3294f5777f
Teodora <garasanin.teodora@gmail.com>
2017-01-04 14:01:54 +0100

    PartyRockApp webView with youtube video fineshed

--- PartyRockApp/PartyRockApp/MainViewController.swift
+++ PartyRockApp/PartyRockApp/MainViewController.swift
@@ -22,7 +22,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let p1 = PartyRock(imageURL:"http://3.bp.blogspot.com/-Ue1BNDfjTwE/UPFybkYOckI/AAAAAAAACB0/SfkwREI3KHM/s640/A-Beautiful-Path-Villaviciosa-Asturias-Spain-700x525.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XmwkjT9oO2E\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "First  Waltz")
         
-           let p2 = PartyRock(imageURL:"http://3.bp.blogspot.com/-Ue1BNDfjTwE/UPFybkYOckI/AAAAAAAACB0/SfkwREI3KHM/s640/A-Beautiful-Path-Villaviciosa-Asturias-Spain-700x525.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XmwkjT9oO2E\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Second Waltz")
+           let p2 = PartyRock(imageURL:"http://3.bp.blogspot.com/-Ue1BNDfjTwE/UPFybkYOckI/AAAAAAAACB0/SfkwREI3KHM/s640/A-Beautiful-Path-Villaviciosa-Asturias-Spain-700x525.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/fhNrqc6yvTU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Second Waltz")
         
           let p3 = PartyRock(imageURL:"http://s1.picswalls.com/wallpapers/2015/12/12/awesome-beautiful-wallpaper_124413582_294.jpg",videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XmwkjT9oO2E\" frameborder=\"0\" allowfullscreen></iframe>",videoTitle: "Third Waltz")
         
@@ -56,5 +56,21 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         return partyRocks.count
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row]
+        
+        performSegue(withIdentifier: "VideoDetailsSegue", sender: partyRock)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? VideoViewController {
+            
+            if let party = sender as? PartyRock {
+             destination.partyRock = party
+            }
+            
+        }
+    }
+    
 }
 
--- PartyRockApp/PartyRockApp/VideoViewController.swift
+++ PartyRockApp/PartyRockApp/VideoViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class VideoViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        }
+        set{
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     }
 
   
