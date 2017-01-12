a7983a2453678ddb557931e3418d905cfce67811
Teodora <garasanin.teodora@gmail.com>
2017-01-12 21:02:26 +0100

    Scroll view. Method touchesBegan finished.

--- Hypnosister/Hypnosister/HypnosisView.h
+++ Hypnosister/Hypnosister/HypnosisView.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface HypnosisView : UIView
+-(void)drawRect:(CGRect)rect;
 
 @end
--- Hypnosister/Hypnosister/ViewController.m
+++ Hypnosister/Hypnosister/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "HypnosisView.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet HypnosisView *hypnosisView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
 
 @end
 
@@ -16,12 +22,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    /*
+    CGRect screenRect = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+    bigRect.size.height *=2.0;
+    
+    */
+    
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+
 
 
 @end
