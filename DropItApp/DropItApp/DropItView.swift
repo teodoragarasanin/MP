4737b33190f99581394e58d1cc6973acd7d35e05
Teodora <garasanin.teodora@gmail.com>
2017-01-14 22:15:33 +0100

    DropItApp - function for removing complete row

--- DropItApp/DropItApp/DropItView.swift
+++ DropItApp/DropItApp/DropItView.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class DropItView: UIView {
+class DropItView: UIView, UIDynamicAnimatorDelegate {
 
     private let dropBehavior = FallingObjectBehavior()
     
-    private lazy var animator: UIDynamicAnimator = UIDynamicAnimator(referenceView: self)
+    private lazy var animator: UIDynamicAnimator = {
+       let animator = UIDynamicAnimator(referenceView: self)
+        animator.delegate = self
+        return animator
+    }()
+    
+    func dynamicAnimatorDidPause(_ animator: UIDynamicAnimator) {
+        removeCompleteRow()
+    }
     
     var animating: Bool = false {
         didSet {
@@ -49,4 +57,83 @@ class DropItView: UIView {
         dropBehavior.addItem(item: drop)
         
     }
+    
+   /*
+    private func removeCompleteRow () {
+        
+        var dropsToRemove = [UIView]()
+        
+        var hitTestRect = CGRect(origin: bounds.lowerLeft, size: dropSize)
+        
+       repeat {
+            hitTestRect.origin.x = bounds.midX
+            hitTestRect.origin.y = hitTestRect.origin.y - dropSize.height
+            var dropsTested = 0
+            var dropsFound = [UIView]()
+            
+            while dropsTested < dropsPerRow {
+                if let hitView = hitTest(p: hitTestRect.mid)   { //, hitView.superview == self
+                    dropsFound.append(hitView)
+                }
+                
+                hitTestRect.origin.x = hitTestRect.origin.x + dropSize.width
+                dropsTested = dropsTested + 1
+                print("\(dropsTested)")
+            }
+            
+            if dropsTested == dropsPerRow {
+                dropsToRemove = dropsToRemove + dropsFound
+                print("\(dropsToRemove.count)")
+            }
+        } while dropsToRemove.count == 0 && hitTestRect.origin.y > bounds.midY
+    
+        for drop in dropsToRemove {
+            dropBehavior.removeItem(item: drop)
+            drop.removeFromSuperview()
+            print("\(dropsToRemove.count)")
+            
+        }
+    }
+    
+ */
+    
+    
+    
+    private func removeCompleteRow () {
+    
+        
+    
+    let yCenterOfBottomView = self.bounds.height - (dropSize.height / 2.0)
+    let xStartPoint = dropSize.width / 2.0
+    let xEndPoint = self.bounds.width
+    
+    var viewsToRemove = [UIView]()
+    
+    for x in stride (from: xStartPoint, to: xEndPoint, by: dropSize.width) {
+        
+        if let topViewInPosition = self.hitTest(CGPoint(x: x, y: yCenterOfBottomView), with: nil) {
+                if topViewInPosition.bounds.size != dropSize {
+                    break
+                }
+    
+            viewsToRemove.append(topViewInPosition)
+        }
+    }
+    
+    if viewsToRemove.count != dropsPerRow {
+        return
+    }
+    
+    for view in viewsToRemove {
+   
+        dropBehavior.removeItem(item: view)
+        view.removeFromSuperview()
+    }
+
+}
+
+    
+    
+
+
 }
--- DropItApp/DropItApp/FallingObjectBehavior.swift
+++ DropItApp/DropItApp/FallingObjectBehavior.swift
@@ -17,19 +17,29 @@ class FallingObjectBehavior: UIDynamicBehavior {
         return collider
     }()
     
+    private let itemBehavior: UIDynamicItemBehavior = {
+        let dib = UIDynamicItemBehavior ()
+        dib.allowsRotation = false
+        dib.elasticity = 0.0
+        return dib
+    }()
+    
     override init() {
         super.init()
         addChildBehavior(gravity)
         addChildBehavior(collider)
+        addChildBehavior(itemBehavior)
     }
     
     func addItem (item: UIDynamicItem) {
         gravity.addItem(item)
         collider.addItem(item)
+        itemBehavior.addItem(item)
     }
     
     func removeItem (item: UIDynamicItem) {
         gravity.removeItem(item)
         collider.removeItem(item)
+        itemBehavior.removeItem(item)
     }
 }
